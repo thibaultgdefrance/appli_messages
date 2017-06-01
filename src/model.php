268b537e8da277efd22da1fb80003257306f7877
Thibault Defrance <thibault.g.defrance@gmail.com>
2017-06-01 15:52:09 +0200

    -function02

--- src/model.php
+++ src/model.php
@@ -1,7 +1,19 @@
 <?php
 function getMessages(){
-    $bdd = new PDO('mysql:host=localhost;dbname=app_messages', 'root', 'u1wj99cv');
+    $bdd = new PDO('mysql:host=localhost;dbname=app_messages', '', '');
 
     $messages = $bdd->query('select * from messages order by id desc');
     return $messages;
+}
+
+function addMessages(){
+    $auteur = isset($_POST['auteur']) ? $_POST['auteur'] : '';
+    $texte = isset($_POST['texte']) ? $_POST['texte'] : '';
+
+    if isset($_POST){
+        bdd_insert ('INSERT INTO message (auteur, texte) VALUES  (:auteur, :texte)', [
+            'auteur'=>$auteur,
+            'texte'=>$texte
+        ]);
+    }
 }
